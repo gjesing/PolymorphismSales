4f2929b6f5f9b5ee458f4979191ebbc33d125d48
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 11:44:07 +0200

    Invoice class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/Invoice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    class Invoice : IPayable
+    {
+        private List<Product> products = new List<Product>();
+
+        protected List<Product> Products { get => products; set => products = value; }
+
+        public decimal GetPaymentAmount()
+        {
+            return products.Sum(product => product.Price);
+        }
+    }
+}
