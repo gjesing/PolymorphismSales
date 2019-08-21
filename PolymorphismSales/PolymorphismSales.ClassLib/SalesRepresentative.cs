800761e9f3d0f1ba07fdec031fcedcea27c4f037
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 13:30:42 +0200

    SalesRepresentative class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/SalesRepresentative.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public class SalesRepresentative : BaseSalariedEmployee
+    {
+        private decimal weeklySales;
+        private double commisionRate;
+
+        public decimal WeeklySales { get => weeklySales; set => weeklySales = value; }
+        public double CommisionRate { get => commisionRate; set => commisionRate = value; }
+
+        public override decimal Earnings()
+        {
+            return base.Earnings();
+        }
+    }
+}
