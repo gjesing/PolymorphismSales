07751cbe5942ef8c70ff5fa8aa25c426351fe63d
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 11:09:52 +0200

    IPayable interface is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/IPayable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public interface IPayable
+    {
+        decimal GetPaymentAmount();
+    }
+}
