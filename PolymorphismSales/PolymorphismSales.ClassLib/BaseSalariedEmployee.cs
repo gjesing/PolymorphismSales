8767ff0475a65e33f1a971b2b5c1014d20c3f4c0
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 13:14:10 +0200

    BaseSalariedEmployee class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/BaseSalariedEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public class BaseSalariedEmployee : Employee
+    {
+        private decimal salary;
+
+        public decimal Salary { get => salary; set => salary = value; }
+
+        public override decimal Earnings()
+        {
+            return salary;
+        }
+    }
+}
