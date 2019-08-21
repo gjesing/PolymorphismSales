2ab8c4b14fa81b9a0dfba0ea29ed74227755fdc5
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 11:57:24 +0200

    Employee class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public abstract class Employee : IPayable
+    {
+        private string name;
+
+        protected string Name { get => name; set => name = value; }
+
+        public abstract decimal Earnings();
+        public decimal GetPaymentAmount()
+        {
+            decimal earnings = Earnings();
+            return earnings - earnings * 0.15m;
+        }
+    }
+}
