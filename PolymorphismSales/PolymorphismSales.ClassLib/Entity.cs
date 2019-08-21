7970edf8d9ad3dcfc82b915bf6d043e63134a277
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 11:12:57 +0200

    Entity class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/Entity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public class Entity
+    {
+        protected int id;
+
+        public int Id { get; }
+    }
+}
