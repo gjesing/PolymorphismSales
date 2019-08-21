f01b81923d8f72410902be16cad1d55b4be6fd04
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-21 11:17:40 +0200

    Product class created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismSales.ClassLib
+{
+    public class Product
+    {
+        private string name;
+        private decimal price;
+        private int quantity;
+
+        public string Name { get => name; set => name = value; }
+        public decimal Price { get => price; set => price = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+    }
+}
