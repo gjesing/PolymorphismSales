b6b25ba26d139deba40b667b8749e06ccb0b913a
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-08-22 09:50:51 +0200

    Unit test to test the Earnings() method in the SalesRepresentative class is now created

---
+++ PolymorphismSales/PolymorphismSales.ClassLib.Tests/SalesRepresentativeTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace PolymorphismSales.ClassLib.Tests
+{
+    public class SalesRepresentativeTests
+    {
+        [Fact]
+        public void Earnings_ReturnsRightAmount()
+        {
+            SalesRepresentative employee = new SalesRepresentative("Name", 20000, 10000, 0.15);
+            decimal expected = employee.Salary + employee.WeeklySales * Convert.ToDecimal(employee.CommisionRate);
+            decimal actual = employee.Earnings();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
